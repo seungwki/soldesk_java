7d0966720baf0a07f6b3388fd5e399cdad1c3614
seungwoo <tmddn2278@naver.com>
2025-08-08 17:52:37 +0900

    0808 : 수업 종료 후 자습

---
+++ java_step1/src/ClassTest/CarrageReturn.java
@@ -0,0 +1,10 @@
+package ClassTest;
+
+public class CarrageReturn {
+	public static void main(String[] args) {
+		for (int i = 0; i < 100000; i++) {
+			System.out.print(i+"\r");
+		}
+		//이클립스라서 안되는거임ㅠㅠ
+	}
+}
